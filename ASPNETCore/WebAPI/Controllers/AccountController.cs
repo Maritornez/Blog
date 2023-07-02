119292799a5bdd3c8ad6532140cf5beb2222f79a
George Potonov <62441435+Maritornez@users.noreply.github.com>
2023-07-02 12:02:41 +0300

    Add files via upload

---
+++ ASPNETCore/WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,130 @@
+﻿using ASPNetCoreApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace ASPNetCoreApp.Controllers
+{
+    [Produces("application/json")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("api/account/register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new() { Email = model.Email, UserName = model.Email };
+                // Добавление нового пользователя
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // установка куки
+                    await _signInManager.SignInAsync(user, false);
+                    return Ok(new { message = "Добавлен новый пользователь: " + user.UserName });
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    var errorMsg = new
+                    {
+                        message = "Пользователь не добавлен.",
+                        error = ModelState.Values.SelectMany(e => e.Errors.Select(er => er.ErrorMessage))
+                    };
+                    return Ok(errorMsg);
+                }
+            }
+            else
+            {
+                var errorMsg = new
+                {
+                    message = "Неверные входные данные.",
+                    error = ModelState.Values.SelectMany(e => e.Errors.Select(er => er.ErrorMessage))
+                };
+                return Ok(errorMsg);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/account/login")]
+        //[AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =
+                    await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return Ok(new { message = "Выполнен вход пользователем: " + model.Email });
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                    var errorMsg = new
+                    {
+                        message = "Вход не выполнен.",
+                        error = ModelState.Values.SelectMany(e => e.Errors.Select(er => er.ErrorMessage))
+                    };
+                    return Ok(errorMsg);
+                }
+            }
+            else
+            {
+                var errorMsg = new
+                {
+                    message = "Вход не выполнен.",
+                    error = ModelState.Values.SelectMany(e => e.Errors.Select(er => er.ErrorMessage))
+                };
+                return Ok(errorMsg);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/account/logoff")]
+        public async Task<IActionResult> LogOff()
+        {
+            User usr = await GetCurrentUserAsync();
+            if (usr == null)
+            {
+                return BadRequest(new { message = "Сначала выполните вход." });
+            }
+            // Удаление куки
+            await _signInManager.SignOutAsync();
+            var msg = new
+            {
+                message = "Выполнен выход."
+            };
+            return Ok(msg);
+        }
+
+        [HttpPost]
+        [Route("api/account/isauthenticated")]
+        public async Task<IActionResult> IsAuthenticated()
+        {
+            User usr = await GetCurrentUserAsync();
+            var message = usr == null ? "Вы Гость. Пожалуйста, выполните вход." : "Вы вошли как: " + usr.UserName;
+            var msg = new
+            {
+                message
+            };
+            return Ok(msg);
+
+        }
+        private Task<User> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+    }
+}
